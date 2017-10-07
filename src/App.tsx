a99f7821f8c44d8c5cf78636e049e0e36dff76f3
lxx871030@gmail.com <lxx871030@gmail.com>
2017-10-07 22:08:44 +1100

    fix the node scss on windows issue and combine both css and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { Component } from 'react';
-// import './App.css';
-import styles from './styles/site.scss';
+ import './App.css';
+import * as styles from './styles/site.scss';
 
 class App extends Component {
     render() {
         return (
             <div className={styles.appss}>
-                <div className="">
-                    <h2>React/typescript and fusebox ccc</h2>
+                <div className="App-header">
+                    <h2>React/typescript and fusebox demo</h2>
                 </div>
-                <p className="">
+                <p className="App-intro">
                     hello world.
                 </p>
             </div>
